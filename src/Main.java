3d68f09394743d558e644eda2e04a8b282a2e9ac
Rohithv17 <rohithv1396@gmail.com>
2024-02-01 12:35:58 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Person p1 = new Person();
+        AddressBookService s1 = new AddressBookService();
+        s1.setValues(p1);
+        System.out.println(s1.display(p1));
+        AddressBook a1 = new AddressBook();
+        a1.contactList.put(1,p1);
+        System.out.println(a1.contactList);
+    }
+}
